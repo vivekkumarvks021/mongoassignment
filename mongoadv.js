0f30e13b12c3e5ea1703b21d5a633352d67ef46b
Vivek Kumar <vivekkumarvks021@gmail.com>
2020-01-14 19:05:38 +0530

    mongo assignment

--- mongoadv.js
+++ mongoadv.js
@@ -1,9 +1,9 @@
-//1.  mongoimport --db restaurents --collection restaurents json_filename_with_path
+//1.  mongoimport --db restaurants --collection restaurants json_filename_with_path
 
 //5.  db.restaurants.find({borough:"Bronx"}).sort({name:-1}) 
 
-/*10. db.restaurents.aggregate([{$match:{borough:"Bronx",cuisine:{$in:['Italian','Bakery']}}},{$group:{_id:"$cuisine", count:{$sum:1}}}])  */
+/*10. db.restaurants.aggregate([{$match:{borough:"Bronx",cuisine:{$in:['Italian','Bakery']}}},{$group:{_id:"$cuisine", count:{$sum:1}}}])  */
 
-/*11. db.restaurents.aggregate([{$match:{borough:"Brooklyn"}},{$group:{_id:"$cuisine"}}]) */
+/*11. db.restaurants.aggregate([{$match:{borough:"Brooklyn"}},{$group:{_id:"$cuisine"}}]) */
 
-/*19. db.restaurents.update({restaurant_id:"40356018"},{$inc:{"grades.0.score":10}}) */
+/*19. db.restaurants.update({restaurant_id:"40356018"},{$inc:{"grades.0.score":10}}) */
